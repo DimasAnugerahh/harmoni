a6eab53302ae42351b5a79383c9b0b6ffb80bb00
DimasAnugerahh <dimasnaugrah883@gmail.com>
2025-07-15 11:45:05 +0800

    fix: fix toggle sidebar

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -505,9 +505,9 @@
                 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
                     xintegrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN"
                     crossorigin="anonymous"></script>
-                <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js"
+                {{-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js"
                     xintegrity="sha384-+sLIOodYLS7CIrQpBjl+C7nPvqq+FbNUBDunl/OZv93DB7Ln/533i8eOxu9+oY"
-                    crossorigin="anonymous">
+                    crossorigin="anonymous"> --}}
                 </script>
                 <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"></script>
 
