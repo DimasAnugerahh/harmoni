a807465c2a32f86fe1af88c757c0c96b318c59a0
DimasAnugerahh <dimasnaugrah883@gmail.com>
2025-07-21 15:28:50 +0800

    chore: set background color of group and marker

--- resources/views/social_mapping/wilayah.blade.php
+++ resources/views/social_mapping/wilayah.blade.php
@@ -59,94 +59,131 @@
                                         <h3>Peta Kondisi Sosial</h3>
                                         <div id="map"></div>
 
-                                        <script>
-                                            const map = L.map('map').setView([-1.449157, 121.854558], 5);
+        <script>
+            const map = L.map('map').setView([-1.449157, 121.854558], 5);
 
-        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            maxZoom: 19,
-            attribution: '&copy; OpenStreetMap contributors'
-        }).addTo(map);
+            L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                maxZoom: 19,
+                attribution: '&copy; OpenStreetMap contributors'
+            }).addTo(map);
 
-        const geojsonData = {
-            "type": "FeatureCollection",
-            "features": [
-                {
-                    "type": "Feature",
-                    "geometry": {
-                        "type": "Point",
-                        "coordinates": [119.4345067,-5.1361493]
+            const geojsonData = {
+                "type": "FeatureCollection",
+                "features": [
+                    {
+                        "type": "Feature",
+                        "geometry": {
+                            "type": "Point",
+                            "coordinates": [119.4345067,-5.1361493]
+                        },
+                        "properties": {
+                            "popupContent": "Kantor Regional 8 PTPN 1"
+                        }
                     },
-                    "properties": {
-                        "popupContent": "Kantor Regional 8 PTPN 1"
-                    }
-                },
-                {
-                    "type": "Feature",
-                    "geometry": {
-                        "type": "Point",
-                        "coordinates": [119.605531,-5.291201]
+                    {
+                        "type": "Feature",
+                        "geometry": {
+                            "type": "Point",
+                            "coordinates": [119.605531,-5.291201]
+                        },
+                        "properties": {
+                            "popupContent": "Kebun Takalar"
+                        }
                     },
-                    "properties": {
-                        "popupContent": "Kebun Takalar"
-                    }
-                },
-                {
-                    "type": "Feature",
-                    "geometry": {
-                        "type": "Point",
-                        "coordinates": [120.8149204, -2.5631286]
+                    {
+                        "type": "Feature",
+                        "geometry": {
+                            "type": "Point",
+                            "coordinates": [120.8149204, -2.5631286]
+                        },
+                        "properties": {
+                            "popupContent": "kebun Tarengge"
+                        }
                     },
-                    "properties": {
-                        "popupContent": "kebun Tarengge"
-                    }
-                },
-                {
-                    "type": "Feature",
-                    "geometry": {
-                        "type": "Point",
-                        "coordinates": [120.7881781, -2.5251552]
+                    {
+                        "type": "Feature",
+                        "geometry": {
+                            "type": "Point",
+                            "coordinates": [120.7881781, -2.5251552]
+                        },
+                        "properties": {
+                            "popupContent": `<p>- kebun Tomoni</p>
+                                            <p>- luas: 000</p>`
+                        }
                     },
-                    "properties": {
-                        "popupContent": `<p>- kebun Tomoni</p>
-                                        <p>- luas: 000</p>`
-                    }
-                },
-                 {
-                    "type": "Feature",
-                    "geometry": {
-                        "type": "Point",
-                        "coordinates": [119.6477950, -5.2445673]
+                    {
+                        "type": "Feature",
+                        "geometry": {
+                            "type": "Point",
+                            "coordinates": [119.6477950, -5.2445673]
+                        },
+                        "properties": {
+                            "popupContent": `<p>- kebun Lanna Gowa</p>
+                                            <p>- luas: 000</p>`
+                        }
                     },
-                    "properties": {
-                        "popupContent": `<p>- kebun Lanna Gowa</p>
-                                        <p>- luas: 000</p>`
-                    }
-                },
-                 {
-                    "type": "Feature",
-                    "geometry": {
-                        "type": "Point",
-                        "coordinates": [119.5031032, -5.3950136]
+                    {
+                        "type": "Feature",
+                        "geometry": {
+                            "type": "Point",
+                            "coordinates": [119.5031032, -5.3950136]
+                        },
+                        "properties": {
+                            "popupContent": `<p>- kebun Moncongkomba</p>
+                                            <p>- luas: 000</p>`
+                        }
                     },
-                    "properties": {
-                        "popupContent": `<p>- kebun Moncongkomba</p>
-                                        <p>- luas: 000</p>`
-                    }
-                },
-            ]
-        };
+                ]
+            };
 
-        var markers = L.markerClusterGroup();
+            const markers = L.markerClusterGroup({
+                iconCreateFunction: function (cluster) {
+                const count = cluster.getChildCount();
 
-        geojsonData.features.forEach(feature => {
-            const coords = feature.geometry.coordinates;
-            const popupContent = feature.properties.popupContent;
-            const marker = L.marker([coords[1], coords[0]]).bindPopup(popupContent);
-            markers.addLayer(marker);
-        });
+                let color = 'green';
+                if (count > 2 && count <= 4) color = 'orange';
+                else if (count > 5) color = 'red'; 
 
-        map.addLayer(markers);
-                                        </script>
+                return L.divIcon({
+                    html: `<div style="background-color:${color}; border-radius:50%; width:40px; height:40px; display:flex; align-items:center; justify-content:center; color:white; font-weight:bold;">${count}</div>`,
+                    className: 'custom-cluster',
+                    iconSize: [40, 40]
+                });
+                }
+            });
+
+            const greenIcon = L.icon({
+                iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-green.png',
+                shadowUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png',
+                iconSize: [25, 41],
+                iconAnchor: [12, 41],
+                popupAnchor: [1, -34],
+                shadowSize: [41, 41]
+            });
+
+            const redIcon = L.icon({
+                iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-red.png',
+                shadowUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png',
+                iconSize: [25, 41],
+                iconAnchor: [12, 41],
+                popupAnchor: [1, -34],
+                shadowSize: [41, 41]
+            });
+
+            geojsonData.features.forEach(feature => {
+                const coords = feature.geometry.coordinates;
+                const popupContent = feature.properties.popupContent;
+                console.log("Popup Content:", popupContent);
+                if (popupContent.includes('Regional')) {
+                    var marker = L.marker([coords[1], coords[0]],{icon:redIcon}).bindPopup(popupContent);
+                }else{
+                    var marker = L.marker([coords[1], coords[0]],{icon:greenIcon}).bindPopup(popupContent);
+                }
+                markers.addLayer(marker);
+            });
+
+            map.addLayer(markers);
+            </script>
     </body>
 
 </html>
